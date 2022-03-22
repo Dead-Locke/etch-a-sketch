3f0fae4b478ebfaa0601406be332a011836e635b
Dead-Locke <impulse25@ymail.com>
2022-03-22 01:31:38 -0400

    Added coloring functionality and color pickers. - Added the ability to change cell color on mouse click. - Added the ability to click (hold) and drag to change colors on the grid. - Added color picker functionality user pen. - Added color picker for background of the grid - Added color picker for grid dividers.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const grid = document.querySelector(".grid")
-const penColor = document.querySelector('.color');
+const penColor = document.querySelector('.pen-color');
 const toggleGrid = document.querySelector('.toggle');
 const gridSize = document.querySelector('.slider');
-const reset = document.querySelector('.reset')
+const reset = document.querySelector('.reset');
+const background = document.querySelector('.background');
+const gridColor = document.querySelector('.grid-color')
+var mouseDownToggle = false;
 
 function createGrid(col,rows) {
     for(let i = 0; i < (col*rows); i++){
@@ -13,7 +16,7 @@ function createGrid(col,rows) {
     }
 }
 
-function reSet(){ //remove all cells in the grid to reset
+function clear(){ //remove all cells in the grid to reset
     const cells = grid.querySelectorAll('.cell');
     cells.forEach(c => c.remove())
 }
@@ -28,15 +31,50 @@ function main (){
     })
 
     gridSize.addEventListener('change', () => {
-        reSet();
+        clear();
         createGrid(gridSize.value, gridSize.value);
+        enableColoring();
     })
 
     reset.addEventListener('click', () => {
         window.location.reload()
     })
-
-
 }
 
+function enableColoring(){
+    const cells = document.querySelectorAll('.cell');
+
+    function colorFill(cell, color){ // match user selected color
+        cell.style.background = color;
+    };
+
+    cells.forEach(cell => { //cell colloring events
+        cell.addEventListener('mousedown', () => {
+            mouseDownToggle = true;
+            colorFill(cell, penColor.value);
+        }); //color in on mouse click
+
+        cell.addEventListener('mouseenter', () => {
+            if(mouseDownToggle) colorFill(cell, penColor.value)
+        }); //if mouse is clicked, dragging will color cells
+        
+        cell.addEventListener('mouseup', () => {
+            mouseDownToggle = false;
+        }); //remove drag coloring when mouse is unclicked
+
+    });
+
+    background.addEventListener('change', () => { //background color
+        cells.forEach(cell => cell.style.background = background.value)
+    })
+
+
+    gridColor.addEventListener('change', () => {
+        cells.forEach(cell => cell.style.borderColor = gridColor.value)
+    })
+
+
+};
+
 main();
+enableColoring();

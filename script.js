f6daaa8c5a73a43d07ba3e567adb0d4739e0a205
Dead-Locke <impulse25@ymail.com>
2022-03-24 00:07:15 -0400

    Grid tool adjustments. Made grid cells tranparent. Background color now changes for the whole grid element. Toggle grid now saves state through grid resizing and clearing. Max grid size reduced to 80x80 as higher values caused a lot of lag.

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-const grid = document.querySelector(".grid")
-const penColor = document.querySelector('.pen-color');
-const toggleGrid = document.querySelector('.toggle');
-const gridSize = document.querySelector('.slider');
+
+const penColor = document.querySelector('#penColor');
+const toggleGrid = document.querySelector('#toggle');
+const gridSize = document.querySelector('#slider');
 const reset = document.querySelector('.reset');
-const gridBackground = document.querySelector('.background');
-const gridColor = document.querySelector('.grid-color')
-const gridSizeNumbers = document.querySelectorAll('.grid-size-number');
+const clearGrid = document.querySelector('.clear');
+const gridBackground = document.querySelector('#background');
+const gridColor = document.querySelector('#gridColor');
+
 var mouseDownToggle = false;
 var rightMouseToggle = false;
 
+//grid creation
+const grid = document.querySelector("#grid")
 function createGrid(col,rows) {
+    
     for(let i = 0; i < (col*rows); i++){
         const div = document.createElement('div')
         grid.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
@@ -18,74 +22,93 @@ function createGrid(col,rows) {
     }
 }
 
-function clear(){ //remove all cells in the grid to reset
+createGrid(20,20);
+enableColoring();
+
+
+//remove all cells in the grid to reset
+function clear(){ 
     const cells = grid.querySelectorAll('.cell');
     cells.forEach(c => c.remove())
 }
 
-createGrid(16,16);
+// toggle gridlines 
+var gridToggle = false;
+toggleGrid.addEventListener('click', ()=> {
+    const cells = document.querySelectorAll('.cell');
+    gridToggle = !gridToggle; //toggle the boolean marker
+    cells.forEach(x => x.classList.toggle('noBorder'));
+})
 
-function main (){
+// resize the grid
+const gridSizeNumbers = document.querySelectorAll('.grid-size-number');
+gridSize.addEventListener('input', () => {
+    clear();
+    createGrid(gridSize.value, gridSize.value);
+    enableColoring();
+    gridSizeNumbers.forEach(x => x.innerText = gridSize.value);
+    retainGrid();
+})
 
-    toggleGrid.addEventListener('click', ()=> {
-        const cells = document.querySelectorAll('.cell');
-        cells.forEach(x => x.classList.toggle('noBorder'));
-    })
+//reset the page
+reset.addEventListener('click', () => {
+    window.location.reload()
+});
 
-    gridSize.addEventListener('input', () => {
-        clear();
-        createGrid(gridSize.value, gridSize.value);
-        enableColoring();
-        gridSizeNumbers.forEach(x => x.innerText = gridSize.value);
-    })
+//clear the grid while retaining settings
+clearGrid.addEventListener('click', () => {
+    clear();
+    createGrid(gridSize.value, gridSize.value);
+    enableColoring();
+    retainGrid()
+});
 
-    reset.addEventListener('click', () => {
-        window.location.reload()
-    })
-}
+// retain grid toggle and color
+function retainGrid (){
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => cell.style.borderColor = gridColor.value);
+    if(gridToggle) cells.forEach(x => x.classList.add('noBorder'))
+};
 
+// main cell coloring function
+function colorFill(cell, color){ 
+    cell.style.background = color;
+};
+
+//event listeners for coloring
 function enableColoring(){
     const cells = document.querySelectorAll('.cell');
 
-    function colorFill(cell, color){ // main coloring function
-        cell.style.background = color;
-    };
+        cells.forEach(cell => { //cell coloring events
+            cell.addEventListener('mousedown', () => {
+                mouseDownToggle = true;
+                colorFill(cell, penColor.value);
+            }); //color in on left mouse click
+            
+            cell.addEventListener('contextmenu', () => {
+                rightMouseToggle = true;
+                colorFill(cell, gridBackground.value);
+            }); //'erase' on right mouse click
 
-    cells.forEach(cell => { //cell coloring events
-        cell.addEventListener('mousedown', () => {
-            mouseDownToggle = true;
-            colorFill(cell, penColor.value);
-        }); //color in on left mouse click
-        
-        cell.addEventListener('contextmenu', () => {
-            rightMouseToggle = true;
-            colorFill(cell, gridBackground.value);
-        }); //'erase' on right mouse click
+            cell.addEventListener('mouseenter', () => {
+                if(mouseDownToggle) colorFill(cell, penColor.value); //left mouse
+                if(rightMouseToggle) colorFill(cell, gridBackground.value) //right mouse
+            }); // dragging will color cells
+            
+            cell.addEventListener('mouseup', () => {
+                mouseDownToggle = false;
+                rightMouseToggle = false;
+            }); //remove drag coloring when mouse is unclicked
 
-        cell.addEventListener('mouseenter', () => {
-            if(mouseDownToggle) colorFill(cell, penColor.value); //left mouse
-            if(rightMouseToggle) colorFill(cell, gridBackground.value) //right mouse
-        }); // dragging will color cells
-        
-        cell.addEventListener('mouseup', () => {
-            mouseDownToggle = false;
-            rightMouseToggle = false;
-        }); //remove drag coloring when mouse is unclicked
+        });
 
-    });
+        //background color
+        gridBackground.addEventListener('input', () => { 
+            grid.style.background = gridBackground.value
+        });
 
-    gridBackground.addEventListener('change', () => { //background color
-        cells.forEach(cell => cell.style.background = gridBackground.value)
-    })
-
-
-    gridColor.addEventListener('change', () => { //change grid color
-        cells.forEach(cell => cell.style.borderColor = gridColor.value)
-    })
-
-
-
-};
-
-main();
-enableColoring();
+        //change grid color
+        gridColor.addEventListener('input', () => {
+            cells.forEach(cell => cell.style.borderColor = gridColor.value)
+        })
+    }
